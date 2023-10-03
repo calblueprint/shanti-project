c9459199e3c62fde1ce7875fedb248f83f64cad5
Buyankhuu Tsolmonkhuu <Buyankhuuts@Berkeley.edu>
2023-10-03 14:55:09 -0700

    chore:applied prettier on files that were pulled

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,28 @@
-'use client'
+'use client';
 
-import LoginForm from "./components/LoginForm";
-import { GlobalStyle, Fullscreen, LoginBox, LoginContent, WelcomeSign, Button } from "./screens/loginScreen/styles"
-
+import LoginForm from './components/LoginForm';
+import {
+  GlobalStyle,
+  Fullscreen,
+  LoginBox,
+  LoginContent,
+  WelcomeSign,
+  Button,
+} from './screens/loginScreen/styles';
 
 export default function App() {
-    return (
-        <main>
-            <GlobalStyle />
-            <Fullscreen>
-                <LoginBox>
-                    <LoginContent>
-                        <WelcomeSign>Welcome</WelcomeSign>
-                        <LoginForm />
-                        <Button>Log In</Button>
-                    </LoginContent>
-                </LoginBox>
-            </Fullscreen>
-        </main >
-    )
+  return (
+    <main>
+      <GlobalStyle />
+      <Fullscreen>
+        <LoginBox>
+          <LoginContent>
+            <WelcomeSign>Welcome</WelcomeSign>
+            <LoginForm />
+            <Button>Log In</Button>
+          </LoginContent>
+        </LoginBox>
+      </Fullscreen>
+    </main>
+  );
 }
--- src/app/screens/profile /profileScreen.tsx
+++ src/app/screens/profile /profileScreen.tsx
@@ -1,7 +1,7 @@
 export default function Profile() {
-    return (
-      <main>
-        <div>Profile</div>
-      </main>
-    );
-  }
+  return (
+    <main>
+      <div>Profile</div>
+    </main>
+  );
+}
