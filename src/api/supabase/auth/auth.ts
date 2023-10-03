8a72bf5ed27067cd6a4d1229b8d16a97ecf8025a
Celine Choi <celinejwchoi@berkeley.edu>
2023-10-02 23:58:12 -0700

    completed auth

---
+++ src/api/supabase/auth/auth.ts
@@ -0,0 +1,20 @@
+import supabase from '@/api/supabase/createClient';
+
+export const handleSignUp = async (email: string, password: string) => {
+    const { data, error } = await supabase.auth.signUp({
+      email,
+      password
+    })
+    console.log(error)
+  }
+
+export const signInWithEmail = async (email: string, password: string) => {
+    const { data, error } = await supabase.auth.signInWithPassword({
+    email, password,
+    })
+    console.log(data)
+}
+
+export const signOut = async () => {
+    const { error } = await supabase.auth.signOut()
+}  
---
+++ src/app/login/page.tsx
@@ -0,0 +1,24 @@
+"use client"
+
+import { useState } from 'react';
+import { handleSignUp, signInWithEmail, signOut } from '@/api/supabase/auth/auth'
+
+export default function Login() {
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+    return (
+    <>
+      <input name="email" onChange={(e) => setEmail(e.target.value)} value={email} />
+      <input
+        type="password"
+        name="password"
+        onChange={(e) => setPassword(e.target.value)}
+        value={password}
+      />
+      <button type="button" onClick={() => handleSignUp(email, password)}>Sign up</button>
+      <button type="button" onClick={() => signInWithEmail(email, password)}>Sign in</button>
+      <button type="button" onClick={signOut}>Sign out</button>
+    </>
+  )
+}
