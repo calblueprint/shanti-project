b666101821f8184f2f9bfc6c8ea08fd9211fa20d
sauhardjain <sauhardjain03@gmail.com>
2023-10-04 22:09:57 -0700

    chore: resolve eslint errors and run prettier

--- src/api/supabase/auth/auth.ts
+++ src/api/supabase/auth/auth.ts
@@ -1,20 +1,21 @@
-import supabase from '@/api/supabase/createClient';
+import supabase from '../createClient';
 
 export const handleSignUp = async (email: string, password: string) => {
-    const { data, error } = await supabase.auth.signUp({
-      email,
-      password
-    })
-    console.log(error)
-  }
+  const { data, error } = await supabase.auth.signUp({
+    email,
+    password,
+  });
+  console.log(error);
+};
 
 export const signInWithEmail = async (email: string, password: string) => {
-    const { data, error } = await supabase.auth.signInWithPassword({
-    email, password,
-    })
-    console.log(data)
-}
+  const { data, error } = await supabase.auth.signInWithPassword({
+    email,
+    password,
+  });
+  console.log(data);
+};
 
 export const signOut = async () => {
-    const { error } = await supabase.auth.signOut()
-}  
+  const { error } = await supabase.auth.signOut();
+};
--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,24 +1,38 @@
-"use client"
+'use client';
 
 import { useState } from 'react';
-import { handleSignUp, signInWithEmail, signOut } from '@/api/supabase/auth/auth'
+import {
+  handleSignUp,
+  signInWithEmail,
+  signOut,
+} from '../../api/supabase/auth/auth';
 
 export default function Login() {
-    const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('')
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
-    return (
+  return (
     <>
-      <input name="email" onChange={(e) => setEmail(e.target.value)} value={email} />
+      <input
+        name="email"
+        onChange={e => setEmail(e.target.value)}
+        value={email}
+      />
       <input
         type="password"
         name="password"
-        onChange={(e) => setPassword(e.target.value)}
+        onChange={e => setPassword(e.target.value)}
         value={password}
       />
-      <button type="button" onClick={() => handleSignUp(email, password)}>Sign up</button>
-      <button type="button" onClick={() => signInWithEmail(email, password)}>Sign in</button>
-      <button type="button" onClick={signOut}>Sign out</button>
+      <button type="button" onClick={() => handleSignUp(email, password)}>
+        Sign up
+      </button>
+      <button type="button" onClick={() => signInWithEmail(email, password)}>
+        Sign in
+      </button>
+      <button type="button" onClick={signOut}>
+        Sign out
+      </button>
     </>
-  )
+  );
 }
