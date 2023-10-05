9891ab4acc2554726b1baba32e634bf27946d193
Casey Lei <63774760+caseyzlei@users.noreply.github.com>
2023-10-04 20:10:13 -0700

    nav between landing and login

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
+import Link from "next/link";
+
 export default function Checkout() {
     return (
       <main>
-        <div>Landing Page</div>
+        <Link href="/login">Login</Link>
       </main>
     );
   }
