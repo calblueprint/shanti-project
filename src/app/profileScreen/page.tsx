e03ba518b6d9c2e481bd7cbac2767982060bb2b1
Buyankhuu Tsolmonkhuu <Buyankhuuts@Berkeley.edu>
2023-10-06 16:24:03 -0700

    refactor:changed folder name

--- src/app/profileScreen/page.tsx
+++ src/app/profileScreen/page.tsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 export default function Profile() {
   return (
     <main>
@@ -5,3 +6,25 @@ export default function Profile() {
     </main>
   );
 }
+=======
+'use client';
+import { LogOutButton, GlobalStyle } from './style';
+import { createClient } from '@supabase/supabase-js';
+
+export default function Profile() {
+  return (
+    <main>
+      <GlobalStyle />
+      <LogOutButton onClick={() => signOut()}>Sign Out</LogOutButton>
+    </main>
+  );
+}
+
+async function signOut() {
+  const supabase = createClient(
+    'https://raqpvvgsmwarxhaialcz.supabase.co',
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJhcXB2dmdzbXdhcnhoYWlhbGN6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTYxODg2MTgsImV4cCI6MjAxMTc2NDYxOH0.triHI7DNSzNeAibtktBZ1b2wQuwnd0WL7R_yZYOJbPc',
+  );
+  const { error } = await supabase.auth.signOut();
+}
+>>>>>>> 22db8ee (refactor:changed folder name)
