b1ea1fe2591bdd5253957ce3cf39be198a6735ff
Celine Choi <celinejwchoi@berkeley.edu>
2023-10-08 00:53:55 -0700

    pulled from main and added files from previous branch

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { useState } from 'react';
 import Link from 'next/link';
 import LoginForm from '../../components/LoginForm';
 
@@ -12,6 +13,13 @@ import {
   WelcomeSign,
   Button,
 } from './styles';
+
+import {
+  handleSignUp,
+  signInWithEmail,
+  signOut,
+} from '../../api/supabase/auth/auth';
+
 
 export default function App() {
   return (
@@ -30,5 +38,35 @@ export default function App() {
         </LoginBox>
       </Fullscreen>
     </main>
+  );
+}
+
+export function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  return (
+    <>
+      <input
+        name="email"
+        onChange={e => setEmail(e.target.value)}
+        value={email}
+      />
+      <input
+        type="password"
+        name="password"
+        onChange={e => setPassword(e.target.value)}
+        value={password}
+      />
+      <button type="button" onClick={() => handleSignUp(email, password)}>
+        Sign up
+      </button>
+      <button type="button" onClick={() => signInWithEmail(email, password)}>
+        Sign in
+      </button>
+      <button type="button" onClick={signOut}>
+        Sign out
+      </button>
+    </>
   );
 }
