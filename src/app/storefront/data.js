1516bba971a4ade8a5efc8882750990020c6721b
Buyankhuu Tsolmonkhuu <Buyankhuuts@Berkeley.edu>
2023-10-08 16:16:32 -0700

    chore:committing  rebase

---
+++ src/app/storefront/data.js
@@ -0,0 +1,18 @@
+export const buttons = [
+  {
+    name: 'All',
+    value: 'all',
+  },
+  {
+    name: 'Fire',
+    value: 'fire',
+  },
+  {
+    name: 'Normal',
+    value: 'normal',
+  },
+  {
+    name: 'Electric',
+    value: 'eletric',
+  },
+];
--- src/app/storefront/page.tsx
+++ src/app/storefront/page.tsx
@@ -1,7 +1,49 @@
-export default function Storefront() {
+import React, { useState, useEffect } from 'react';
+import './styles.css';
+import { buttons } from './data';
+import { getPokemon, filterPokemon } from '../services/services';
+
+export function getPokemon() {
+  const pokemonList = pokemons;
+  return pokemonList;
+}
+
+export function filterPokemon(pokeType) {
+  let filtredPokemon = getPokemon().filter(type => type.tipo === pokeType);
+  return filtredPokemon;
+}
+
+//Dog, Cat, Supplies
+export default function App() {
+  const [filtredPokemon, setFiltredProducts] = useState(null);
+  useEffect(() => {
+    setFiltredProducts(getPokemon());
+  }, []);
+
+  function handlePokemon(e) {
+    let typePokemon = e.target.value;
+    typePokemon !== 'all'
+      ? setFiltredPokemon(filterPokemon(typePokemon))
+      : setFiltredPokemon(getPokemon());
+  }
+
   return (
-    <main>
-      <div>storefront</div>
-    </main>
+    <>
+      {buttons &&
+        buttons.map((type, index) => (
+          <>
+            <button key={index} value={type.value} onClick={handlePokemon}>
+              {type.name}
+            </button>
+          </>
+        ))}
+
+      {filtredPokemon &&
+        filtredPokemon.map(type => (
+          <ul key={type.id}>
+            <li>{type.nome}</li>
+          </ul>
+        ))}
+    </>
   );
 }
