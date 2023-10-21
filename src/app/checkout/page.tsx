068ee294dd9e513d415c0aa4e49ab906bb0a3785
Casey Lei <63774760+caseyzlei@users.noreply.github.com>
2023-10-21 15:34:33 -0700

    trying conditional nav

--- src/app/checkout/page.tsx
+++ src/app/checkout/page.tsx
@@ -1,7 +1,34 @@
+'use client';
+
+import { useRouter } from "next/navigation";
+import supabase from "@/api/supabase/createClient";
+import { Button } from "../login/styles";
+
+const { data: { user } } = await supabase.auth.getUser();
+console.log(user);
+const userDelivery = user;
+const { data, error } = await supabase
+  .from('profiles')
+  .select('delivery_enabled')
+  .eq('user_id', user?.id)
+
 export default function Checkout() {
+  const router = useRouter();
+
+  const checkDelivery = () => {
+    if (userDelivery) {
+      router.push('/delivery');
+    } else {
+      router.push('/pickup');
+    }
+  };
+
   return (
     <main>
-      <div>Checkout</div>
+      <Button onClick={(checkDelivery)}>
+              Checkout
+      </Button>
+      {/* <div>Checkout</div> */}
     </main>
   );
 }
