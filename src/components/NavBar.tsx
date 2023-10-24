9717c37c612e041dc8b5115e154ba2aa2f22e02e
Buyankhuu Tsolmonkhuu <Buyankhuuts@Berkeley.edu>
2023-10-24 14:43:11 -0700

    chore:fixing prettier

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react';
 
-import { NavBarComp, ButtonsDiv } from "../styles/components";
+import { NavBarComp, ButtonsDiv } from '../styles/components';
 
 export default function NavBar() {
   return (
