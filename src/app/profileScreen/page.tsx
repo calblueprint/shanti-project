b2a4d2ea0c456919b40388cf05a2e355d65cbfc8
Buyankhuu Tsolmonkhuu <Buyankhuuts@Berkeley.edu>
2023-10-24 00:08:03 -0700

    feaT:navBar

--- src/app/profileScreen/page.tsx
+++ src/app/profileScreen/page.tsx
@@ -3,10 +3,11 @@
 import { LogOutButton, GlobalStyle } from './style';
 
 import { signOut } from '../../api/supabase/auth/auth';
-
+import NavBar from '@/components/NavBar';
 export default function Profile() {
   return (
     <main>
+      <NavBar />
       <GlobalStyle />
       <LogOutButton onClick={() => signOut()}>Sign Out</LogOutButton>
     </main>
