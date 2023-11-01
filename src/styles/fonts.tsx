ded8f83d9b1d789c5ca2590183379dc8baca440b
EthanAuyeung <ethana2442@gmail.com>
2023-11-01 16:58:06 -0700

    added some fonts

--- src/styles/fonts.tsx
+++ src/styles/fonts.tsx
@@ -1,3 +1,17 @@
-const FONTS = {};
+import styled from 'styled-components/native';
 
-export default FONTS;
+export const Heading1 = styled.Text`
+    font-family: public-sans;
+    font-size: 40px;
+    font-style: normal;
+    font-weight: 700;
+    line-height: normal;
+`;
+
+export const Heading4 = styled.Text`
+    font-family: public-sans;
+    font-size: 15px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: normal;
+`
