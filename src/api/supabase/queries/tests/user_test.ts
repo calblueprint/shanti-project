25ecea32ab06abccb7091251e92d4339108c436d
kevinjcai <75354434+kevinjcai@users.noreply.github.com>
2023-11-05 16:18:16 -0800

    Update user_test.ts

--- src/api/supabase/queries/tests/user_test.ts
+++ src/api/supabase/queries/tests/user_test.ts
@@ -1,12 +1,6 @@
-/* eslint-disable no-console */
-//
 
-import {
-  fetchUserData,
-  
-} from '../user_queries';
 async default function runFetchUserData() {
   
-    const result = await fetchUserData();
+    const result = 5
   
 }
