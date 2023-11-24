a7089d5775224cba20286145780d1a13b2afd41f
Buyankhuu Tsolmonkhuu <Buyankhuuts@Berkeley.edu>
2023-11-24 13:49:36 -0800

    buyankhuu:fixed eslint and prettier

--- src/app/storefront/productButtons.tsx
+++ src/app/storefront/productButtons.tsx
@@ -57,21 +57,21 @@ export default function ProductButtons(props: {
   async function applyFilter(
     e: React.MouseEvent<HTMLButtonElement, MouseEvent>,
   ) {
-    //which button is clicked
+    // which button is clicked
 
     const category = e.currentTarget.value;
 
     for (let i = 0; i < buttons.length; i += 1) {
       if (buttons[i].value === category) {
-        let ind = buttons[i].count;
+        const ind = buttons[i].count;
         if (IsClickedButton[ind] === true) {
-          let tempArray = [...IsClickedButton];
+          const tempArray = [...IsClickedButton];
           tempArray[ind] = !tempArray[ind];
           tempArray[0] = true;
           setCategoryWord('All');
           setIsClickedButton(tempArray);
         } else {
-          let arrayOfFalse = [false, false, false, false];
+          const arrayOfFalse = [false, false, false, false];
           arrayOfFalse[ind] = true;
           setCategoryWord(buttons[i].value);
           setIsClickedButton(arrayOfFalse);
@@ -81,9 +81,9 @@ export default function ProductButtons(props: {
       }
     }
 
-    //Changing the Color of the Button
+    // Changing the Color of the Button
 
-    //Applying the filter to the categories of the product
+    // Applying the filter to the categories of the product
 
     if (category !== 'All') {
       const products = await filterProduct(category);
