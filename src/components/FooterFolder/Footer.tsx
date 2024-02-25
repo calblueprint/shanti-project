56ef43f49d26fee6e9d68ff2acd51b7350355244
CharlotteLaw <CharlotteLaw@berkeley.edu>
2024-02-25 15:27:59 -0800

    feat: Footer

--- src/components/FooterFolder/Footer.tsx
+++ src/components/FooterFolder/Footer.tsx
@@ -1,31 +1,128 @@
-import Location from './Location';
+import { Body1 } from '@/styles/fonts';
 
-import { FooterDiv, ContactDiv } from './styles';
+import Image from 'next/image';
+import { useRouter } from 'next/navigation';
+
+import {
+  MySpanIcon,
+  WordDiv,
+  WordDiv1,
+  YoutubeIcon,
+  FacebookIcon,
+  InstagramIcon,
+  Body1New,
+  Body2New,
+  Heading,
+  SVGWrapper,
+  MySpan,
+  Wrapper,
+  LinkDiv,
+  ContactDiv,
+  MissionDiv,
+  Body1Bold,
+  ArrowUpRightIcon,
+} from './styles';
 
 export default function Footer({ ...rest }) {
+  const router = useRouter();
   return (
     <main {...rest}>
-      <FooterDiv>
+      <Wrapper>
         <ContactDiv>
-          <h1>Contact Us</h1>
+          <Heading>Contact Us</Heading>
+          <Body1New>Phone: (415) 674-4700</Body1New>
+          <SVGWrapper>
+            <Body1
+              style={{ cursor: 'pointer' }}
+              onClick={() => {
+                window.location.href =
+                  'https://www.facebook.com/ShantiProjectSF/';
+              }}
+            >
+              <Body1>
+                <MySpanIcon>
+                  <FacebookIcon />
+                </MySpanIcon>
+              </Body1>
+            </Body1>
+            <Body1
+              style={{ cursor: 'pointer' }}
+              onClick={() => {
+                window.location.href =
+                  'https://www.youtube.com/channel/UCc3DMrL7_KDOzeJNVkoFrsA';
+              }}
+            >
+              <Body1>
+                <MySpanIcon>
+                  <YoutubeIcon />
+                </MySpanIcon>
+              </Body1>
+            </Body1>
+            <Body1
+              style={{ cursor: 'pointer' }}
+              onClick={() => {
+                window.location.href =
+                  'https://www.instagram.com/shantiprojectsf/';
+              }}
+            >
+              <Body1>
+                <MySpanIcon>
+                  <InstagramIcon />
+                </MySpanIcon>
+              </Body1>
+            </Body1>
+          </SVGWrapper>
         </ContactDiv>
-        <Location
-          District="Tenderloin"
-          Address="730 Polk Street, 3rd Floor San Francisco, CA 9410"
-          Phone="Phone: (415) 674-4700"
-        />
 
-        <Location
-          District="Mission District"
-          Address="3170 23rd Street San Francisco, CA 94110"
-          Phone="Phone: (415) 674-4700"
+        <MissionDiv>
+          <Body1Bold>Mission District</Body1Bold>
+          <WordDiv>
+            <Body1New>3170 23rd Street</Body1New>
+            <Body1New>San Francisco, CA 94110</Body1New>
+          </WordDiv>
+          <WordDiv1>
+            <Body2New
+              style={{ cursor: 'pointer' }}
+              onClick={() => {
+                window.location.href =
+                  'https://maps.app.goo.gl/LJWvkdhwrRMhjEZs7';
+              }}
+            >
+              <MySpan>SEE ON MAP</MySpan>
+            </Body2New>
+            <ArrowUpRightIcon />
+          </WordDiv1>
+        </MissionDiv>
+
+        <LinkDiv>
+          <Body1New
+            style={{ cursor: 'pointer' }}
+            onClick={() => router.push('/storefront')}
+          >
+            <MySpan>Shop</MySpan>
+          </Body1New>
+          <Body1New
+            style={{ cursor: 'pointer' }}
+            onClick={() => router.push('/profileScreen')}
+          >
+            <MySpan>Profile</MySpan>
+          </Body1New>
+          <Body1New
+            style={{ cursor: 'pointer' }}
+            onClick={() => router.push('/cart')}
+          >
+            <MySpan>Cart</MySpan>
+          </Body1New>
+        </LinkDiv>
+
+        <Image
+          src="/images/PAWS.png"
+          alt="logo pic"
+          width={233}
+          height={162}
+          style={{ marginTop: '48px', marginBottom: '48px' }}
         />
-        <Location
-          District="Potrero Hill"
-          Address="950 Connecticut St San Francisco, CA 94107"
-          Phone="Phone: (415) 674-4700"
-        />
-      </FooterDiv>
+      </Wrapper>
     </main>
   );
 }
