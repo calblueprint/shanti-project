c8d8b2e772ceb7b1c5aba1be6e1039c14f661b04
Buyankhuu Tsolmonkhuu <Buyankhuuts@Berkeley.edu>
2024-03-14 00:59:50 -0700

    rebase1

--- src/api/supabase/queries/order_queries.ts
+++ src/api/supabase/queries/order_queries.ts
@@ -1,12 +1,9 @@
 /* eslint-disable no-console */
 //
 
-import {
-  Order,
-  OrderProduct,
-  ProductWithQuantity,
-} from '../../../schema/schema';
+import { Order, OrderProduct, Product } from '../../../schema/schema';
 import { fetchUser } from './user_queries';
+import { fetchProductByID } from './product_queries';
 import supabase from '../createClient';
 
 /**
@@ -103,6 +100,12 @@ export async function fetchNOrdersByUserIdSorted(n: number): Promise<Order[]> {
   return sortOrdersByCreated(orders).slice(0, n);
 }
 
+export async function fetchOrderIdsByUserIdSorted(): Promise<number[]> {
+  const ordersProm = await fetchOrdersByUser();
+  const orders = sortOrdersByCreated(ordersProm);
+  return orders.map(order => order.id);
+}
+
 export async function fetchOrderProductById(
   productId: number,
 ): Promise<OrderProduct> {
@@ -117,36 +120,27 @@ export async function fetchOrderProductById(
   return orderProduct;
 }
 
-export async function fetchProductWithQuantityById(
-  productId: number,
-): Promise<ProductWithQuantity> {
-  const { data: orderProduct, error } = await supabase
-    .from('product')
-    .select('*')
-    .eq('id', productId)
-    .single();
-  if (error) {
-    throw new Error(`Error fetching order product: ${error.message}`);
-  }
-  return orderProduct;
+export async function fetchProductFromOrderProduct(
+  orderProductId: number,
+): Promise<Product> {
+  const orderProduct = await fetchOrderProductById(orderProductId);
+  const product = await fetchProductByID(orderProduct.product_id);
+  return product;
 }
 
-export async function fetchRecentOrderProducts(): Promise<OrderProduct[]> {
-  const order = await fetchNOrdersByUserIdSorted(1);
-  const orderProductIds = order[0].order_product_id_array;
+export async function fetchProductsFromOrder(
+  orderId: number,
+): Promise<Product[]> {
+  const order = await getOrderById(orderId);
+  const products = order.order_product_id_array;
 
-  const orderProducts = await Promise.all(
-    orderProductIds.map(async orderProductId => {
-      try {
-        const orderProduct = await fetchOrderProductById(orderProductId);
-        return orderProduct;
-      } catch (error) {
-        throw new Error(`Error fetching order product array.`);
-      }
-    }),
-  );
+  const productPromises = products.map(async (productID: number) => {
+    const product = await fetchProductFromOrderProduct(productID);
+    return product;
+  });
+  const fetchedProducts = await Promise.all(productPromises);
 
-  return orderProducts;
+  return fetchedProducts;
 }
 
 export async function fetchOrderProductsbyOrderId(
