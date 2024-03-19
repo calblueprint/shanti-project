4a1e09006800c77e2ef4bc8f0142de965696ad78
Kevin Cai <kevincai@berkeley.edu>
2024-03-19 11:18:41 -0700

    REbase

--- src/api/supabase/queries/order_queries.ts
+++ src/api/supabase/queries/order_queries.ts
@@ -1,12 +1,9 @@
 /* eslint-disable no-console */
 //
 
-import {
-  Order,
-  OrderProduct,
-  ProductWithQuantity,
-} from '../../../schema/schema';
+import { Order, OrderProduct , Product } from '../../../schema/schema';
 import { fetchUser } from './user_queries';
+import { fetchProductByID } from './product_queries';
 import supabase from '../createClient';
 
 /**
@@ -83,6 +80,7 @@ export async function fetchOrdersByUser(): Promise<Order[]> {
   return data;
 }
 
+
 /**
  * gets all orders by user id and sorted it by creation data
  * @param Order[] - An array of Order objects.
@@ -103,6 +101,12 @@ export async function fetchNOrdersByUserIdSorted(n: number): Promise<Order[]> {
   return sortOrdersByCreated(orders).slice(0, n);
 }
 
+export async function fetchOrderIdsByUserIdSorted(): Promise<number[]> {
+  const ordersProm = await fetchOrdersByUser();
+  const orders = sortOrdersByCreated(ordersProm);
+  return orders.map(order => order.id);
+}
+
 export async function fetchOrderProductById(
   productId: number,
 ): Promise<OrderProduct> {
@@ -117,9 +121,10 @@ export async function fetchOrderProductById(
   return orderProduct;
 }
 
+
 export async function fetchProductWithQuantityById(
   productId: number,
-): Promise<ProductWithQuantity> {
+): Promise<Product> {
   const { data: orderProduct, error } = await supabase
     .from('product')
     .select('*')
@@ -131,27 +136,29 @@ export async function fetchProductWithQuantityById(
   return orderProduct;
 }
 
-export async function fetchRecentOrderProducts(): Promise<OrderProduct[]> {
-  const order = await fetchNOrdersByUserIdSorted(1);
-  const orderProductIds = order[0].order_product_id_array;
+export async function fetchProductFromOrderProduct(orderProductId: number): Promise<Product>{
+  const orderProduct = await fetchOrderProductById(orderProductId);
+  const product = await fetchProductByID(orderProduct.product_id);
+  return product;
 
-  const orderProducts = await Promise.all(
-    orderProductIds.map(async orderProductId => {
-      try {
-        const orderProduct = await fetchOrderProductById(orderProductId);
-        return orderProduct;
-      } catch (error) {
-        throw new Error(`Error fetching order product array.`);
-      }
-    }),
-  );
+}
 
-  return orderProducts;
+export async function fetchProductsFromOrder(orderId: number):Promise<Product[]> {
+  const order = await getOrderById(orderId);
+  const products = order.order_product_id_array;
+
+  const productPromises = products.map(async (productID: number) => {
+    const product = await fetchProductFromOrderProduct(productID);
+    return product;
+  });
+  const fetchedProducts = await Promise.all(productPromises);
+
+  return fetchedProducts;
 }
 
 export async function fetchOrderProductsbyOrderId(
   orderId: number,
-): Promise<ProductWithQuantity[]> {
+): Promise<Profuct[]> {
   const order = await getOrderById(orderId);
   const orderProductIds = order.order_product_id_array;
 
@@ -167,7 +174,7 @@ export async function fetchOrderProductsbyOrderId(
     ),
   );
 
-  return orderProducts;
+  return fetchedProducts;
 }
 
 /**
