07fd138cec8373bba18fb494d96acf1ea4450464
David Qing <davidqing6432@gmail.com>
2024-03-26 17:20:29 -0700

    cleaned up the profile screen, more to be done

--- src/components/BackButton/styles.ts
+++ src/components/BackButton/styles.ts
@@ -1,26 +1,17 @@
 import styled from 'styled-components';
 import { ArrowLeft } from 'react-feather';
-import Image from 'next/image';
+import Link from 'next/link';
 import COLORS from '../../styles/colors';
 
-export const BackDiv = styled.button`
+export const BackLink = styled(Link)`
   display: flex;
   flex-direction: row;
   align-items: center;
   color: ${COLORS.black};
-  background-color: transparent;
-  border: transparent;
-  margin-left: 0px;
-  margin-top: 55px;
   gap: 5px;
   &:hover {
     text-decoration: underline;
   }
-`;
-
-export const ImageWithSize = styled(Image)`
-    width={23}
-    height={23}
 `;
 
 export const ArrowLeftIcon = styled(ArrowLeft)`
