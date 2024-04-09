9ae5b591164291b81ff80a0c9b7a28b4dc3e8681
Buyankhuu Tsolmonkhuu <Buyankhuuts@Berkeley.edu>
2024-04-08 19:58:13 -0700

    done

--- src/app/storefront/page.tsx
+++ src/app/storefront/page.tsx
@@ -43,7 +43,7 @@ export default function App() {
       <StorefrontBox>
         <Storefront products={FilteredProducts} />
       </StorefrontBox>
-     
+
       <Footer />
     </Fullscreen>
   );
